591ff684154ffc40d7f447115e90b3a56be5eb6f
Rashid-Khan-Github <128183393+Rashid-Khan-Github@users.noreply.github.com>
2023-05-12 15:50:20 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,13 +1,13 @@
 def buildApp() {
-    echo "Building the application"
+    echo 'Building the application'
 }
 
 def bestApp() {
-    echo "Testing the application"
+    echo 'Testing the application'
 }
 
 def deployApp() {
-    echo "Deploying the application"
-    echo "Deploying version"
+    echo 'Deploying the application'
+    echo 'Deploying version'
 }
 return this
