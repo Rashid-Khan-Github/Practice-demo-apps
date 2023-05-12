5750df613f710ab19280f144f39dc48b476466e1
Rashid-Khan-Github <rashidkhan7805@gmail.com>
2023-05-12 18:22:39 +0530

    added new

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+    echo 'Building the application'
+}
+
+def bestApp() {
+    echo 'Testing the application'
+}
+
+def deployApp() {
+    echo 'Deploying the application'
+}
+
+return this
